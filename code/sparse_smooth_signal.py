48b0f0c24f8a0f069a8a2ed39c9b4e070f8c96e0
David <david.rochinhachaves@epfl.ch>
2022-02-19 21:52:48 +0100

    add draw fonction to SpareSmoothSignal, plus some modification

--- code/sparse_smooth_signal.py
+++ code/sparse_smooth_signal.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 from scipy.linalg import dft
-from scipy.sparse import random as sparse_random
+from scipy import sparse
 import matplotlib.pyplot as plt
 
 
@@ -16,7 +16,7 @@ class SpareSmoothSignal:
     """
 
     def __init__(self, dim: (int, int), sparse: None | np.ndarray = None, smooth: None | np.ndarray = None,
-                 measurement_operator: None | np.ndarray = None):
+                 measurement_operator: None | np.ndarray = None, variance: float = 1):
         assert dim[0] >= 0 and dim[1] >= 0, "Negative dimension is not valid"
 
         self.__dim = dim
@@ -25,13 +25,13 @@ class SpareSmoothSignal:
 
         if sparse is not None:
             assert sparse.shape == dim, "Sparse is not the same shape as dim"
-            self.__sparse = sparse
+            self.__sparse = sparse.ravel()
         else:
             self.random_sparse()
 
         if smooth is not None:
             assert smooth.shape == dim, "Smooth is not the same size as dim"
-            self.__smooth = smooth
+            self.__smooth = smooth.ravel()
         else:
             self.random_smooth()
 
@@ -39,9 +39,10 @@ class SpareSmoothSignal:
             assert measurement_operator.shape[1] == self.__size, "Measurement operator shape does not match dim"
             self.__measurement_operator = measurement_operator
         else:
-            self.__measurement_operator = None
-
+            self.random_measurement_operator(self.__y_size)
+        self.__variance = variance
         self.__noise = None
+        self.gaussian_noise()
 
     @property
     def dim(self) -> (int, int):
@@ -58,10 +59,6 @@ class SpareSmoothSignal:
     @property
     def measurement_operator(self) -> None | np.ndarray:
         return self.__measurement_operator
-
-    @property
-    def sigma(self) -> None | np.float_:
-        return self.__sigma
 
     @property
     def x(self) -> None | np.ndarray:
@@ -83,11 +80,19 @@ class SpareSmoothSignal:
             return self.y0 + self.__noise
         return None
 
+    @property
+    def noise(self) -> np.ndarray:
+        return self.__noise
+
     def random_sparse(self) -> None:
-        self.__sparse = np.ravel(sparse_random(self.__dim[0], self.__dim[1]).toarray())
+        self.__sparse = sparse.random(self.__dim[0], self.__dim[1], density=0.25, data_rvs=np.random.randn)\
+            .toarray().ravel()
 
     def random_smooth(self) -> None:
-        self.__smooth = np.random.rand(self.__dim[0], self.__dim[1]).ravel()
+        amp = np.random.randint(0, 5, 2)
+        freq = 2*np.pi*np.random.rand(2)
+        t = np.linspace(0, self.__size/10, self.__size)
+        self.__smooth = amp[0]*np.sin(t/freq[0]) + amp[1]*np.cos(t/freq[1])
 
     def random_measurement_operator(self, size: int) -> None:
         assert self.__size >= size >= 0
@@ -97,6 +102,33 @@ class SpareSmoothSignal:
         self.__measurement_operator = dft_mtx[rand]
         self.__noise = None
 
-    def gaussian_noise(self, sigma=1) -> None:
-        self.__noise = np.random.normal(0, sigma, self.__y_size)
+    def gaussian_noise(self, variance: float = None) -> None:
+        if variance is None:
+            variance = self.__variance
+        else:
+            self.__variance = variance
+        self.__noise = np.random.normal(0, variance, self.__y_size)
 
+    def draw_sparse(self) -> None:
+        plt.plot(self.sparse, label="sparse")
+
+    def draw_smooth(self) -> None:
+        plt.plot(self.smooth, label="smooth")
+
+    def draw_x(self) -> None:
+        plt.plot(self.x, label="x")
+
+    def draw_y0(self) -> None:
+        plt.plot(self.y0, label="y0")
+
+    def draw_y(self) -> None:
+        plt.plot(self.y, label="y")
+
+    def draw_noise(self) -> None:
+        plt.plot(self.noise, label="noise")
+
+    def show(self) -> None:
+        plt.title("Spare + Smooth Signal")
+        plt.ylabel("Amplitude")
+        plt.legend(loc="best")
+        plt.show()
