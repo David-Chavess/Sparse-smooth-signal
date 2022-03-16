251434c7a2484e9fe729aa3666cf1863933d127b
davidrochinhachaves <david.rochinhachaves@epfl.ch>
2022-03-16 15:40:47 +0100

    Tikhonov solver now solver for x1 and x2

--- src/tikhonov_solver.py
+++ src/tikhonov_solver.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import numpy as np
-from pycsou.func import SquaredL2Loss, L2Norm, QuadraticForm
-from pycsou.linop import DenseLinearOperator
-from pycsou.opt import APGD, PrimalDualSplitting
+from pycsou.func import SquaredL2Loss, QuadraticForm, DiffFuncHStack, NullDifferentiableFunctional
+from pycsou.linop import DenseLinearOperator, IdentityOperator, LinOpHStack
+from pycsou.opt import APGD
 
 from src.solver import Solver, MyOperator
 
@@ -13,10 +13,8 @@ class TikhonovSolver(Solver):
     def __init__(self, y: np.ndarray, operator: np.ndarray, tikhonov_matrix: None | float | np.ndarray,
                  generalized: bool = False) -> None:
         super().__init__(y, operator)
-        if type(tikhonov_matrix) is float:
-            self.tikhonov_matrix = tikhonov_matrix * np.eye(operator.shape[1])
-        else:
-            self.tikhonov_matrix = tikhonov_matrix
+
+        self.tikhonov_matrix = tikhonov_matrix
         self.generalized = generalized
 
     def solve(self) -> (np.ndarray, np.ndarray):
@@ -24,18 +22,26 @@ class TikhonovSolver(Solver):
         H = MyOperator(self.operator)
         H.compute_lipschitz_cst()
 
+        stack = LinOpHStack(H, H, n_jobs=-1)
+        stack.compute_lipschitz_cst()
+
         l22_loss = (1 / 2) * SquaredL2Loss(H.shape[0], self.y)
-        F = l22_loss * H
+        F = l22_loss * stack
 
-        D = DenseLinearOperator(self.tikhonov_matrix)
-        D.compute_lipschitz_cst()
+        if self.tikhonov_matrix is not None:
+            if self.generalized:
+                L = QuadraticForm(H.shape[1], DenseLinearOperator(self.tikhonov_matrix))
+            else:
+                if type(self.tikhonov_matrix) is float:
+                    L = QuadraticForm(H.shape[1], self.tikhonov_matrix * IdentityOperator(H.shape[1]))
+                else:
+                    L = QuadraticForm(H.shape[1], DenseLinearOperator(self.tikhonov_matrix.T @ self.tikhonov_matrix))
 
-        if self.generalized:
-            L = QuadraticForm(H.shape[1], D)
-        else:
-            L = L2Norm(H.shape[1])
+            F = F + DiffFuncHStack(NullDifferentiableFunctional(H.shape[1]), L)
 
-        apgd = PrimalDualSplitting(self.operator.shape[1], F=F, H=L, K=D, verbose=None)
-        estimate, converged, diagnostics = apgd.iterate()
-        x = estimate['primal_variable']#['iterand']
-        return x, x
+        pds = APGD(2 * self.operator.shape[1], F=F, verbose=None)
+        estimate, converged, diagnostics = pds.iterate()
+        x = estimate['iterand']
+        x1 = x[:self.operator.shape[1]]
+        x2 = x[self.operator.shape[1]:]
+        return x1, x2
