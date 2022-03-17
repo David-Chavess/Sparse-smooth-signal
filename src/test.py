7657190150dd875bdc19ddc37b1565d55df34a22
davidrochinhachaves <david.rochinhachaves@epfl.ch>
2022-03-17 07:45:18 +0100

    update tests

--- src/test.py
+++ src/test.py
@@ -1,6 +1,7 @@
 from pycsou.linop import FirstDerivative
 
 from src.lasso_solver import LassoSolver
+from src.sparse_smooth_solver import SparseSmoothSolver
 from src.tikhonov_solver import TikhonovSolver
 from src.sparse_smooth_signal import SparseSmoothSignal
 import numpy as np
@@ -14,13 +15,15 @@ class Test:
     def test_operator(self) -> None:
         operator = SparseSmoothSignal.create_measurement_operator(self.dim)
         vec = np.random.randint(1000, size=self.dim)
-        assert np.allclose((operator @ vec.ravel()).reshape(self.dim), np.fft.fft2(vec))
+        assert np.allclose((operator @ vec.ravel()).reshape(self.dim), np.fft.fft2(vec, norm='ortho'))
 
     def test_noise(self) -> None:
-        s = SparseSmoothSignal(self.dim)
-        s.gaussian_noise(80)
+        s = SparseSmoothSignal(self.dim, psnr=80)
         psnr = 20 * np.log10(np.max(np.abs(s.y0))) - 10 * np.log10(np.var(s.y - s.y0))
-        assert np.allclose(psnr, 80, rtol=0.1)
+        assert np.allclose(psnr, 80, atol=0.1)
+        s = SparseSmoothSignal(self.dim, psnr=0)
+        psnr = 20 * np.log10(np.max(np.abs(s.y0))) - 10 * np.log10(np.var(s.y - s.y0))
+        assert np.allclose(psnr, 0, atol=0.1)
 
     def test_cache(self) -> None:
         s = SparseSmoothSignal(self.dim)
@@ -46,27 +49,19 @@ class Test:
 
 
 if __name__ == '__main__':
-    #test = Test()
-    #test.test_operator()
-    #test.test_noise()
-    #test.test_cache()
-    #print("Done")
+    # test = Test()
+    # test.test_operator()
+    # test.test_noise()
+    # test.test_cache()
+    # print("Test Done")
 
-    dim = (100, 100)
-    s = SparseSmoothSignal(dim, measurement_operator=5000)
+    dim = (128, 128)
+    s = SparseSmoothSignal(dim, measurement_operator=64*64)
 
-    sol = TikhonovSolver(s.y, s.measurement_operator, 0.1)
-    x, _ = sol.solve()
+    sol = SparseSmoothSolver(s.y, s.measurement_operator, 0.1, 1)
+    x1, x2 = sol.solve()
 
     s.plot("Base")
-    s.show()
-    SparseSmoothSignal(dim, smooth=x.reshape(dim), sparse=np.zeros(dim), measurement_operator=s.H).plot()
-
-    D = FirstDerivative(10000)
-    D.compute_lipschitz_cst()
-    sol = LassoSolver(s.y, s.measurement_operator, 0.1, D)
-    x, _ = sol.solve()
-
-    SparseSmoothSignal(dim, smooth=np.zeros(dim), sparse=x.reshape(dim), measurement_operator=s.H).plot()
+    SparseSmoothSignal(dim, x1.reshape(dim), x2.reshape(dim), s.H).plot("Sparse + Smooth")
     s.show()
 
