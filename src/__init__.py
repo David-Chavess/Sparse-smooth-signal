2ba49333be04601a1db45d3e2c49111a0ba35880
davidrochinhachaves <david.rochinhachaves@epfl.ch>
2022-04-25 15:38:54 +0200

    fix imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,3 @@
-from src.hyperparameter_testing import *
 from src.lasso_solver import LassoSolver
 from src.solver import MyMatrixFreeOperator
 from src.solver import MyOperator
@@ -6,3 +5,5 @@ from src.solver import Solver
 from src.sparse_smooth_signal import SparseSmoothSignal
 from src.sparse_smooth_solver import SparseSmoothSolver
 from src.tikhonov_solver import TikhonovSolver
+from src.hyperparameter_testing import *
+from src.util import *
