7e25b8aa59103b7adb07607c309e4ae26513cc8a
David <david.rochinhachaves@epfl.ch>
2022-06-09 11:51:54 +0200

    more plotting function

--- src/util.py
+++ src/util.py
@@ -304,6 +304,37 @@ def plot_reconstruction(x_sparse: np.ndarray, x_smooth: np.ndarray, x_reconst_sp
     cbar = fig.colorbar(im_p, cax=cb_ax)
 
 
+def plot_2_reconstruction(sparse: np.ndarray, smooth: np.ndarray, name: str = "") -> None:
+    """
+    Plot the two image, a sparse one and a smooth one
+
+    Parameters
+    ----------
+    sparse : np.ndarray
+        Sparse image to plot
+    smooth : np.ndarray
+        Smooth image to plot
+    name : str
+        Name used as title, usually contains the parameters used in the solver
+    """
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    fig.canvas.manager.set_window_title(f'Spare + Smooth Signal : {name}')
+    fig.suptitle(name)
+
+    im_p = ax1.imshow(sparse, vmin=0, vmax=SparseSmoothSignal.MAX_SPARSE_AMPLITUDE)
+    ax1.axis('off')
+
+    im_s = ax2.imshow(smooth, vmin=0, vmax=SparseSmoothSignal.MAX_SMOOTH_AMPLITUDE)
+    ax2.axis('off')
+
+    fig.subplots_adjust(bottom=0.05, top=0.9, left=0.01, right=0.92,
+                        wspace=0.1, hspace=0.1)
+    cb_ax = fig.add_axes([0.92, 0.05, 0.02, 0.85])
+    cbar = fig.colorbar(im_s, cax=cb_ax)
+    cb_ax = fig.add_axes([0.445, 0.05, 0.02, 0.85])
+    cbar = fig.colorbar(im_p, cax=cb_ax)
+
+
 def plot_reconstruction_measurements(x_sparse: np.ndarray, x_smooth: np.ndarray, x1_sparse: np.ndarray,
                                      x1_smooth: np.ndarray,
                                      x2_sparse: np.ndarray, x2_smooth: np.ndarray, x3_sparse: np.ndarray,
